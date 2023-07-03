60828a550a9784e789b65b01b7b7e109de115eeb
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-03 10:27:00 -0500

    Update home.py

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide", page_title="Elecciones 🇨🇴 2023 🗳️ By Novus Vote", page_icon="🗳️")
 
 st.title('Elecciones Colombia 2023 🇨🇴')
-st.header("Tecnología para Ganar Elecciones by Novus Vote 🗳️")
+st.header("Tecnología para Ganar by Novus Vote 🗳️")
 
 st.write("Centrales de Mando y Asistentes Virtuales serán la clave para una campaña austera pero potente")
 
