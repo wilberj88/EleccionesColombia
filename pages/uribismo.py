a83fe702e4d1fbfe66682797e6a642233f29030f
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-08-01 17:39:46 -0500

    Update uribismo.py

--- pages/uribismo.py
+++ pages/uribismo.py
@@ -1,7 +1,35 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+# Add a title and intro text
+st.title('Earthquake Data Explorer')
+st.text('This is a web app to allow exploration of Earthquake Data')
+
+# Create file uploader object
+upload_file = st.file_uploader('Upload a file containing earthquake data')
+
+# Check to see if a file has been uploaded
+if upload_file is not None:
+    # If it has then do the following:
+
+    # Read the file to a dataframe using pandas
+    df = pd.read_csv(upload_file)
+
+    # Create a section for the dataframe statistics
+    st.header('Statistics of Dataframe')
+    st.write(df.describe())
+
+    # Create a section for the dataframe header
+    st.header('Header of Dataframe')
+    st.write(df.head())
+
+    # Create a section for matplotlib figure
+    st.header('Plot of Data')
+    
+    fig, ax = plt.subplots(1,1)
+    ax.scatter(x=df['Depth'], y=df['Magnitude'])
+    ax.set_xlabel('Depth')
+    ax.set_ylabel('Magnitude')
+    
+    st.pyplot(fig)
