1b155beb6946238b85310e4a50cb410f012665e9
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-08-04 16:04:47 -0500

    Update uribismo.py

--- pages/uribismo.py
+++ pages/uribismo.py
@@ -6,32 +6,47 @@ import matplotlib.pyplot as plt
 st.title('Mando PreCampaña')
 st.text('Alcaldía de Bogotá: históricos, tendencias y proyecciones 2023')
 
-# Create file uploader object
-upload_file = st.file_uploader('Upload a E14 or E24 or E26')
 
-# Check to see if a file has been uploaded
-if upload_file is not None:
-    # If it has then do the following:
-
-    # Read the file to a dataframe using pandas
-    df = pd.read_csv(upload_file)
-    tables = camelot.read_pdf(upload_file)
-    st.write("Total tables extracted:", tables.n)
-    st.write(tables)
-    # Create a section for the dataframe statistics
-    st.header('Statistics of Dataframe')
-    st.write(df.describe())
-
-    # Create a section for the dataframe header
-    st.header('Header of Dataframe')
-    st.write(df.head())
-
-    # Create a section for matplotlib figure
-    st.header('Plot of Data')
-    
-    fig, ax = plt.subplots(1,1)
-    ax.scatter(x=df['Depth'], y=df['Magnitude'])
-    ax.set_xlabel('Depth')
-    ax.set_ylabel('Magnitude')
-    
-    st.pyplot(fig)
+st.title('Contraste Votos Zonas entre Alcalde Actual y Top 3 votos Alcaldía 2019 Vs 2015')
+def render_basic_radar():
+    option = {
+        "title": {"text": "Previo Votación 🗳️"},
+        "legend": {"data": ["Rodrigo Hernandez", "Top2.2019", "Top3.2019"]},
+        "radar": {
+            "indicator": [
+                {"name": "Norte", "max": 10000},
+                {"name": "Sur", "max": 10000},
+                {"name": "Oriente", "max": 10000},
+                {"name": "Occidente", "max": 10000},
+                {"name": "Centro", "max": 10000},
+            ]
+        },
+        "series": [
+            {
+                "name": "Votos por Zonas",
+                "type": "radar",
+                "data": [
+                    {
+                        "value": [5500, 5789, 8450, 5500, 7500],
+                        "name": "Rodrigo Hernandez",
+                    },
+                    {
+                        "value": [3500, 4500, 2500, 3500, 7000],
+                        "name": "Top2.2019",
+                    },
+                    {
+                        "value": [3000, 1500, 2000, 1800, 3000],
+                        "name": "Top3.2019",
+                    },
+                ],
+            }
+        ],
+    }
+    st_echarts(option, height="500px")
+ST_RADAR_DEMOS = {
+    "Radar: Basic Radar": (
+        render_basic_radar,
+        "https://echarts.apache.org/examples/en/editor.html?c=radar",
+    ),
+}
+render_basic_radar()
